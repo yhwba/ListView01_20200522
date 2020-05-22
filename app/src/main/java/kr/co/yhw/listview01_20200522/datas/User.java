8ab651f84ae0d710567bbb4d276fe0889a292d44
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-22 10:39:30 +0900

    User 클래스 추가  - getter /setter (private 변수)  - constructor (생성) 추가  - Alt + Insert 단축키로 자동작성

---
+++ app/src/main/java/kr/co/yhw/listview01_20200522/datas/User.java
@@ -0,0 +1,31 @@
+package kr.co.yhw.listview01_20200522.datas;
+
+public class User {
+
+    private String name;
+    private String address;
+
+//    alt +insert
+
+
+    public User(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
