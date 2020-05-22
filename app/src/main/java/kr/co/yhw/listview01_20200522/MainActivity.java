cd766d5804fa6e29fbc05a7eca5215ec8e8fbfd3
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-22 11:34:00 +0900

    수강생 담는 메쏘드 별도 작성 addStudents

--- app/src/main/java/kr/co/yhw/listview01_20200522/MainActivity.java
+++ app/src/main/java/kr/co/yhw/listview01_20200522/MainActivity.java
@@ -32,6 +32,10 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        addStudents();
+    }
+
+    void addStudents(){
         ourClassStudentList.add(new Student("조경진", "서울시 은평구"));
         ourClassStudentList.add(new Student("강희진", "인천시 남동구"));
         ourClassStudentList.add(new Student("구현정", "서울시 종로구"));
