26e99894a12e630592fbe7051f1f0ec94cbd1100
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-22 10:20:56 +0900

    프로젝트 기본 셋팅

---
+++ app/src/main/java/kr/co/yhw/listview01_20200522/BaseActivity.java
@@ -0,0 +1,14 @@
+package kr.co.yhw.listview01_20200522;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+
+}
